6181748da962b711f2f59dbd418e6c5b9049c61e
FelipeSaadi <felipesaadisiegert@hotmail.com>
2021-07-07 20:01:53 -0300

    added js study

---
+++ JS/Fundamentos_1/assignment.js
@@ -0,0 +1,15 @@
+let country = "Brasil";
+let continent = "América do Sul";
+let population = "212 milhões";
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
+let isIsland = false;
+let language;
+
+console.log(typeof country);
+console.log(typeof population);
+console.log(typeof isIsland);
+console.log(typeof language);
---
+++ JS/Fundamentos_1/script.js
@@ -0,0 +1,36 @@
+/*
+let js = "amazing";
+console.log(40 + 8 + 23 - 10);
+
+let firsName = "Felipe";
+console.log(firsName);
+
+let age = 22;
+console.log(age);
+
+let PI = 3.1415;
+
+let myFirstJob = "Programmer";
+
+console.log(myFirstJob);
+*/
+
+/*
+let javascriptIsFun = true;
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 22);
+console.log(typeof "Felipe");
+
+javascriptIsFun = "YES";
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1998;
+console.log(typeof year);
+
+console.log(typeof null);
+*/
