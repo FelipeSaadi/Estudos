926c425a60245642a35abba9d773b7e35ef491a1
FelipeSaadi <felipesaadisiegert@hotmail.com>
2021-07-10 03:20:03 -0300

    added challenge file and fishined the first

---
+++ JS/Fundamentos_1/challenge.js
@@ -0,0 +1,33 @@
+let markMass, markHeight, markBMI, markHigherBMI;
+let johnMass, johnHeight, johnBMI;
+
+markMass = 78;
+markHeight = 1.69;
+markBMI = markMass / markHeight ** 2;
+
+johnMass = 92;
+johnHeight = 1.95;
+johnBMI = johnMass / johnHeight ** 2;
+
+console.log(markBMI, johnBMI);
+
+markHigherBMI = markBMI > johnBMI;
+
+console.log(markHigherBMI);
+
+markMass = 95;
+markHeight = 1.88;
+markBMI = markMass / markHeight ** 2;
+
+johnMass = 85;
+johnHeight = 1.76;
+johnBMI = johnMass / johnHeight ** 2;
+
+console.log(markBMI, johnBMI);
+
+markHigherBMI = markBMI > johnBMI;
+
+console.log(markHigherBMI);
+
+
+
