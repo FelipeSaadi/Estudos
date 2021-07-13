14e85aafc82d186a1211b4f0e1cd48d5190b3517
FelipeSaadi <felipesaadisiegert@hotmail.com>
2021-07-13 03:13:12 -0300

    completed the challenge 3

--- JS/Fundamentos_1/challenge.js
+++ JS/Fundamentos_1/challenge.js
@@ -1,4 +1,5 @@
 /*
+// Challenge 1
 let markMass, markHeight, markBMI, markHigherBMI;
 let johnMass, johnHeight, johnBMI;
 
@@ -22,6 +23,8 @@ if (markHigherBMI) {
     console.log(`John's BMI (${johnBMI.toFixed(1)}) is higher than Mark's (${markBMI.toFixed(1)})!`);
 }
 
+// Challenge 2
+
 markMass = 95;
 markHeight = 1.88;
 markBMI = markMass / markHeight ** 2;
@@ -40,5 +43,32 @@ if (markHigherBMI) {
     console.log(`Mark's (${markBMI.toFixed(1)}) is higher than John's (${johnBMI.toFixed(1)})!`);
 } else {
     console.log(`John's (${johnBMI.toFixed(1)}) is higher than Mark's (${markBMI.toFixed(1)})!`);
+}
+
+// Challenge 3
+
+let dolphins = 0, koalas = 0, game = 0;
+let gameDolphinsPts = [97, 112, 101];
+let gameKoalasPts = [109, 95, 106];
+
+
+while (game < 3) {
+    compete(gameDolphinsPts[game], gameKoalasPts[game])
+    game++
+    if (game === 3) {
+        dolphins = dolphins / game;
+        koalas = koalas / game;
+    }
+}
+
+console.log(dolphins, koalas)
+if (dolphins > koalas && dolphins >= 100) console.log("Dolphins wins the trophy!");
+else if (dolphins < koalas && koalas >= 100) console.log("Koalas wins the trophy!");
+else if (dolphins === koalas && dolphins >= 100) console.log("Both win the trophy!");
+else console.log("No team wins the trophy")
+
+function compete(dolphinsPts, koalasPts) {
+    dolphins += dolphinsPts;
+    koalas += koalasPts;
 }
 */
