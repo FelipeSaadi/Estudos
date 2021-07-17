60fddc010c014a13b1f43929bef32c2215e319c7
FelipeSaadi <felipesaadisiegert@hotmail.com>
2021-07-17 02:29:32 -0300

    completed the Function assignment

---
+++ JS/Fundamentals_2/assignments.js
@@ -0,0 +1,17 @@
+"use strict";
+
+/*
+function describeCountry(country, population, capitalCity) {
+    const countryDescription = `${country} has ${population} million people and its capital city is ${capitalCity}`;
+    return countryDescription;
+}
+
+const countryOneDescription = describeCountry("Brazil", 212, "Brasília");
+console.log(countryOneDescription);
+
+const countryTwoDescription = describeCountry("USA", 326, "Washington");
+console.log(countryTwoDescription);
+
+const countryThreeDescription = describeCountry("Japan", 125, "Tokyo");
+console.log(countryThreeDescription);
+*/
