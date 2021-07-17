b9cd3e1c92612bb07edbfd9cd69d7e5b19717c27
FelipeSaadi <felipesaadisiegert@hotmail.com>
2021-07-17 16:22:01 -0300

    completed the Function Declarations vs Expressions assignment

--- JS/Fundamentals_2/assignments.js
+++ JS/Fundamentals_2/assignments.js
@@ -14,4 +14,38 @@ console.log(countryTwoDescription);
 
 const countryThreeDescription = describeCountry("Japan", 125, "Tokyo");
 console.log(countryThreeDescription);
+
+const worldPopulation = 7900;
+let countryName, countryPopulation;
+
+// Function Declaration
+function percentageOfWorld1(country, population) {
+    countryName = country;
+    countryPopulation = population;
+    return (population / worldPopulation * 100).toFixed(1);
+}
+const chinaWorldPercent1 = percentageOfWorld1("China", 1441);
+console.log(`${countryName} has ${countryPopulation} million people, so it's about ${chinaWorldPercent1}% of the world population`);
+
+const brazilWorldPercent1 = percentageOfWorld1("Brazil", 212);
+console.log(`${countryName} has ${countryPopulation} million people, so it's about ${brazilWorldPercent1}% of the world population`);
+
+const usaWorldPercent1 = percentageOfWorld1("USA", 326);
+console.log(`${countryName} has ${countryPopulation} million people, so it's about ${usaWorldPercent1}% of the world population`);
+
+// Function Expression
+const percentageOfWorld2 = function (country, population) {
+    countryName = country;
+    countryPopulation = population;
+    return (population / worldPopulation * 100).toFixed(1);
+}
+
+const chinaWorldPercent2 = percentageOfWorld2("China", 1441);
+console.log(`${countryName} has ${countryPopulation} million people, so it's about ${chinaWorldPercent2}% of the world population`);
+
+const brazilWorldPercent2 = percentageOfWorld2("Brazil", 212);
+console.log(`${countryName} has ${countryPopulation} million people, it's about ${brazilWorldPercent2}% of the world population`);
+
+const usaWorldPercent2 = percentageOfWorld2("USA", 326);
+console.log(`${countryName} has ${countryPopulation} million people, it's about ${usaWorldPercent2}% of the world population`);
 */
