575def067640db7e915e7fc898ada352e684a880
FelipeSaadi <felipesaadisiegert@hotmail.com>
2021-07-16 21:58:26 -0300

    completed the Activating Strict Mode lesson

---
+++ JS/Fundamentals_2/script.js
@@ -0,0 +1,11 @@
+"use strict";
+/*
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriverLicense = true;
+if (hasDriversLicense) console.log("You can drive :D");
+
+// const interface = "Áudio";
+// const private = 537;
+*/
