ff5958d39fedc02d7973bc69d9e08a58e3b3f093
FelipeSaadi <felipesaadisiegert@hotmail.com>
2021-07-17 02:17:58 -0300

    completed the Function lesson

--- JS/Fundamentals_2/script.js
+++ JS/Fundamentals_2/script.js
@@ -8,4 +8,28 @@ if (hasDriversLicense) console.log("You can drive :D");
 
 // const interface = "Áudio";
 // const private = 537;
+
+function logger() {
+    console.log("My name is Felipe");
+}
+
+logger();
+logger();
+logger();
+
+function fruitProcessor(apple, orange) {
+    const juice = `Juice with ${apple} apples and ${orange} oranges.`;
+    return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const orangeJuice = fruitProcessor(0, 5);
+console.log(orangeJuice);
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+
+const num = Number("22");
 */
