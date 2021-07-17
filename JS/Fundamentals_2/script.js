eae9a9a519d66fefc01011e9befe0e70f952f3d9
FelipeSaadi <felipesaadisiegert@hotmail.com>
2021-07-17 20:25:54 -0300

    completed the Functions Calling Other Functions lesson

--- JS/Fundamentals_2/script.js
+++ JS/Fundamentals_2/script.js
@@ -57,4 +57,17 @@ const yearsUntilRetirement = (birthYear, firstName) => {
 
 console.log(yearsUntilRetirement(1998, "Felipe"));
 console.log(yearsUntilRetirement(2000, "Sarah"));
+
+function cutFruitPieces(fruit) {
+    return fruit * 4;
+}
+
+function fruitProcessor(apples, oranges) {
+    const applePieces = cutFruitPieces(apples);
+    const orangePieces = cutFruitPieces(oranges);
+
+    const juice = `Juice with ${applePieces} pieces of apple and ${orangePieces} pieces of orange.`;
+    return juice;
+}
+console.log(fruitProcessor(2, 5));
 */
