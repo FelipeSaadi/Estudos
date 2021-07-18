d3a11a9b9f1d92d7233b5e2266acf51c831268c9
FelipeSaadi <felipesaadisiegert@hotmail.com>
2021-07-18 02:04:36 -0300

    completed the Coding Challenge #1

---
+++ JS/Fundamentals_2/challenges.js
@@ -0,0 +1,27 @@
+"use strict";
+
+/*
+// Coding Challenge 1
+const calcAvarage = (score1, score2, score3) => (score1 + score2 + score3) / 3;
+
+// Test 1
+let avgDolphins = calcAvarage(44, 23, 71);
+let avgKoalas = calcAvarage(65, 54, 49);
+
+const checkWinner = (avgDolphins, avgKoalas) => {
+    if (avgDolphins >= avgKoalas * 2) {
+        console.log(`Dolphins win (${avgDolphins * 3} vs. ${avgKoalas * 3})`);
+    } else if (avgKoalas >= avgDolphins * 2) {
+        console.log(`Koalas win (${avgKoalas} vs. ${avgDolphins})`);
+    } else {
+        console.log("No team wins");
+    }
+}
+checkWinner(avgDolphins, avgKoalas);
+
+// Test 2
+avgDolphins = calcAvarage(85, 54, 41);
+avgKoalas = calcAvarage(23, 34, 27);
+
+checkWinner(avgDolphins, avgKoalas);
+*/
