00f6040a03a07478b02e050054cba7e959745bee
FelipeSaadi <felipesaadisiegert@hotmail.com>
2021-07-18 00:49:38 -0300

    completed the Reviewing Functions lesson

--- JS/Fundamentals_2/script.js
+++ JS/Fundamentals_2/script.js
@@ -70,4 +70,24 @@ function fruitProcessor(apples, oranges) {
     return juice;
 }
 console.log(fruitProcessor(2, 5));
+
+const calcAge = function (birthYear) {
+    return 2037 - birthYear;
+}
+
+const yearsUntilRetirement = function (birthYear, firstName) {
+    const age = calcAge(birthYear);
+    const retirement = 65 - age;
+
+    if (retirement > 0) {
+        console.log(`${firstName} retires in ${retirement} years`);
+        return retirement;
+    } else {
+        console.log(`${firstName} has already retired`);
+        return -1;
+    }
+}
+
+console.log(yearsUntilRetirement(1960, "Felipe"));
+console.log(yearsUntilRetirement(2000, "Sarah"));
 */
