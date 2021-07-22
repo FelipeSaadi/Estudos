6fadf14421cf49d0e5aff33d989f63465a81da85
FelipeSaadi <felipesaadisiegert@hotmail.com>
2021-07-21 23:53:27 -0300

    completed the Basic Array Operations (Methods) lesson

--- JS/Fundamentals_2/script.js
+++ JS/Fundamentals_2/script.js
@@ -127,4 +127,36 @@ console.log(age1, age2, age3);
 
 const ages = [calcAge(years[0]), calcAge(years[1]), calcAge(years[years.length - 1])];
 console.log(ages);
+
+// Basic Array Operations
+const friends = ["Michael", "Steven", "Peter"];
+
+// Add elements
+const newLength = friends.push("Jay");
+console.log(friends);
+console.log(newLength);
+
+friends.unshift("John");
+console.log(friends);
+
+// Remove elements
+friends.pop(); // Remove the last
+const popped = friends.pop();
+console.log(popped);
+console.log(friends);
+
+friends.shift();
+console.log(friends);
+
+friends.indexOf("Steven");
+friends.indexOf("Bob");
+
+friends.push(23);
+console.log(friends.includes("Steven"));
+console.log(friends.includes("Bob"));
+console.log(friends.includes(23));
+
+if (friends.includes("Steven")) {
+    console.log("You have a friend called Steven");
+}
 */
