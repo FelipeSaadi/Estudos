f015290643146df60b2f10f7c76a75fef70bc7f4
FelipeSaadi <felipesaadisiegert@hotmail.com>
2021-07-24 23:17:45 -0300

    completed the Coding Challenge #3

--- JS/Fundamentals_2/challenges.js
+++ JS/Fundamentals_2/challenges.js
@@ -47,4 +47,32 @@ calcTip(bills[2]);
 
 console.log(tips);
 console.log(total);
+
+// Challenge 3
+const mark = {
+    fullName: "Mark Miller",
+    mass: 78,
+    height: 1.69,
+    calcBMI: function () {
+        this.BMI = (this.mass / this.height ** 2).toFixed(1);
+        return this.BMI;
+    }
+};
+const john = {
+    fullName: "John Smith",
+    mass: 92,
+    height: 1.95,
+    calcBMI: function () {
+        this.BMI = (this.mass / this.height ** 2).toFixed(1);
+        return this.BMI;
+    }
+};
+
+if (mark.calcBMI() > john.calcBMI()) {
+    console.log(`${mark.fullName}'s BMI (${mark.BMI}) is higher than ${john.fullName}'s (${john.BMI}).`);
+} else if (john.BMI > mark.BMI) {
+    console.log(`${john.fullName}'s BMI (${john.BMI}) is higher than ${mark.fullName}'s (${mark.BMI}).`);
+} else {
+    console.log(`The BMI of ${mark.fullName} and ${john.fullName} is equal.`);
+}
 */
