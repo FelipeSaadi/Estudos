c2a94e606b139e21e262960902bcff6dcc847fb3
FelipeSaadi <felipesaadisiegert@hotmail.com>
2021-08-04 21:34:18 -0300

    function renamed

--- next.js/exercises/pages/basics/list1.jsx
+++ next.js/exercises/pages/basics/list1.jsx
@@ -1,12 +1,12 @@
 export default function list1() {
     return (
         <div>
-            {list()}
+            {generateList()}
         </div>
     )
 }
 
-const list = function () {
+const generateList = function () {
     const list = []
     for (let i = 1; i <= 10; i++) {
         if (i === 10) {
