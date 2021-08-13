80844f9877d800b2eb6ea7467e266d64efca2380
FelipeSaadi <felipesaadisiegert@hotmail.com>
2021-08-13 01:00:06 -0300

    improved the Lottery Draw Challeng with Ordered Numbers

--- next.js/exercises/components/LotteryDraw.jsx
+++ next.js/exercises/components/LotteryDraw.jsx
@@ -2,11 +2,22 @@ import LotteryNumber from "./LotteryNumber"
 
 export default function LotteryDraw(props) {
     function generateDraw(draws) {
+        const generatedDraw = []
         const lotteryNumbers = []
+
         for (let i = 0; i < draws; i++) {
-            lotteryNumbers.push(<LotteryNumber key={i} number={Math.floor(Math.random(1) * 61)} />)
+            let number = parseInt(Math.random() * 60) + 1
+            while (lotteryNumbers.includes(number)) {
+                number = parseInt(Math.random() * 60) + 1
+            }
+            lotteryNumbers.push(number)
         }
-        return lotteryNumbers
+        lotteryNumbers.sort((n1, n2) => n1 - n2)
+
+        for (let i = 0; i < draws; i++) {
+            generatedDraw.push(<LotteryNumber key={i} number={lotteryNumbers[i]} />)
+        }
+        return generatedDraw
     }
     return (
         <div style={{
