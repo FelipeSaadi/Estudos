99d0cb3b8e47dd83cdfc1a4f1b000c67fcd36024
FelipeSaadi <felipesaadisiegert@hotmail.com>
2021-09-02 01:59:49 -0300

    created the logic of the row component creation

--- Projetos/React/first-application/src/components/Home.tsx
+++ Projetos/React/first-application/src/components/Home.tsx
@@ -1,29 +1,47 @@
 import { Modal, Button } from "antd"
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import 'antd/dist/antd.css'
 import styles from "../css/home.module.css"
 import TableHeader from "./table/TableHeader"
 import TableRow from "./table/TableRow"
 export default () => {
+    const create = (teste: any,) => {
+        return [...teste, (
+            <TableRow title={title} price={value} category={category} date={date} />)]
+    }
+    const clearInputs = () => {
+        setTitle('')
+        setValue('')
+        setCategory('')
+        setDate('')
+    }
+    // Inputs
+    const [title, setTitle] = useState<any>()
+    const [value, setValue] = useState<any>()
+    const [category, setCategory] = useState<any>()
+    const [date, setDate] = useState<any>()
+
     // Table
-    const [table, setTable] = useState([])
+    const [table, setTable] = useState<Array<any>>([])
 
     // ModaL
     const [isModalVisible, setIsModalVisible] = useState(false)
 
     const showModal = () => {
-
+        clearInputs()
         setIsModalVisible(true);
     }
 
     const handleOk = () => {
-        /*setTable((table) => {
-            [...table, "teste"]
-        })*/
-        setIsModalVisible(false);
+        if (title && value && category && date) {
+            setTable(create(table))
+            setIsModalVisible(false);
+            clearInputs()
+        }
     }
 
     const handleCancel = () => {
+        clearInputs()
         setIsModalVisible(false);
     }
 
@@ -32,25 +50,23 @@ export default () => {
             <div className={styles.container}>
                 <div className={styles.displayButton}>
                     <button onClick={showModal} className={styles.addNewButton}>New Entry</button>
-                    <Modal title="New Entry" centered visible={isModalVisible} onOk={handleOk} onCancel={handleCancel}>
-                        <label className={styles.modalLabel} htmlFor="title">Title</label>
-                        <input className={styles.modalInput} type="text" />
-                        <label className={styles.modalLabel} htmlFor="value">Value</label>
-                        <input className={styles.modalInput} type="number" />
-                        <label className={styles.modalLabel} htmlFor="category">Category</label>
-                        <input className={styles.modalInput} type="text" />
-                        <label className={styles.modalLabel} htmlFor="date">Date</label>
-                        <input className={styles.modalInput} type="date" />
+                    <Modal title="New Entry" centered visible={isModalVisible} okType="default" onOk={handleOk} onCancel={handleCancel}>
+                        <form id="form" action="">
+                            <label className={styles.modalLabel} htmlFor="title">Title</label>
+                            <input value={title} onChange={(ev) => setTitle(ev.target.value)} id="title" className={styles.modalInput} type="text" required />
+                            <label className={styles.modalLabel} htmlFor="value">Value</label>
+                            <input value={value} onChange={(ev) => setValue(ev.target.value)} id="value" className={styles.modalInput} type="number" required />
+                            <label className={styles.modalLabel} htmlFor="category">Category</label>
+                            <input value={category} onChange={(ev) => setCategory(ev.target.value)} id="category" className={styles.modalInput} type="text" required />
+                            <label className={styles.modalLabel} htmlFor="date">Date</label>
+                            <input value={date} onChange={(ev) => setDate(ev.target.value)} id="date" className={styles.modalInput} type="date" required />
+                        </form>
+
                     </Modal>
                 </div>
                 <TableHeader />
                 <div className={styles.table}>
-                    <TableRow title="Deposit" price={2000.00} category="Others" date="22/06/2021" />
-                    <TableRow title="Gasoline" price={-40} category="Others" date="26/07/2021" />
-                    <TableRow title="Paid Out" price={-500} category="Others" date="22/06/2021" />
-                    <TableRow title="Restaurant" price={-80} category="Food" date="15/08/2021" />
-                    <TableRow title="Restaurant" price={-480} category="Food" date="15/08/2021" />
-                    <TableRow title="Restaurant" price={980} category="Food" date="15/08/2021" />
+                    {table}
                 </div>
             </div>
         </div>
