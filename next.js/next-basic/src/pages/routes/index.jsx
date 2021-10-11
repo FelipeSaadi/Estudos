847c0366a3562c8fcca124d8d8c55da80bfcb6bd
FelipeSaadi <felipesaadisiegert@hotmail.com>
2021-10-11 19:36:04 -0300

    completed the lesson Navegation into Pages #02 from Next.js

--- next.js/next-basic/src/pages/routes/index.jsx
+++ next.js/next-basic/src/pages/routes/index.jsx
@@ -1,6 +1,20 @@
 import Link from 'next/link'
+import router from 'next/router'
 
 export default function routes() {
+  function navegationSimple(url) { 
+    router.push(url)
+  }
+  function navegationWithParams() {
+    router.push({
+      pathname: "/routes/params",
+      query: {
+        id: 15,
+        name: "Nath"
+      }
+    })
+  }
+
   return (
     <div>
       <h1>Routes index</h1>
@@ -15,6 +29,11 @@ export default function routes() {
           <li>Rick</li>
         </Link>
       </ul>
+      <div style={{ display: "flex", flexDirection: "column", alignItems: "flex-start"}}>
+        <button onClick={navegationWithParams}>Params</button>
+        <button onClick={() => router.push("/routes/123/find")}>Find</button>
+        <button onClick={() => navegationSimple("/routes/123/Rick")}>Rick</button>
+      </div>
     </div>
   )
 }
