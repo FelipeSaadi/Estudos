f58b5694e6e7f411728b42b34a9fca9f9efe71c7
Felipe Saadi <54749257+FelipeSaadi@users.noreply.github.com>
2022-05-19 00:21:15 -0300

    Aulas inicias de node
    Console, globals, require e módulos do node.

---
+++ Inteli/node/exports.js
@@ -0,0 +1,1 @@
+module.exports = "Dados enviados de outro arquvivo."
---
+++ Inteli/node/require.js
@@ -0,0 +1,9 @@
+// Módulos nativos
+// const path = require("path")
+
+// console.log(path.basename("./globals.js"))
+
+// Módulos criados
+const newData = require("./exports")
+
+console.log(newData)
