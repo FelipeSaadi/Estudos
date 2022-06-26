082e5f16263cf363a3a11c0315aa851776812e40
Felipe Saadi <54749257+FelipeSaadi@users.noreply.github.com>
2022-06-26 19:43:22 -0300

    Criação de Componentes

--- React/Modulo 1/react-vite-first/src/App.tsx
+++ React/Modulo 1/react-vite-first/src/App.tsx
@@ -1,39 +1,11 @@
+import Header from './components/Header'
+import Calc from './components/Calc'
+
 const App = () => {
-  let name: string = "Felipe";
-  let lastName: string = "Saadi Siegert";
-  let n1: number = 10;
-  let n2: number = 3;
-  let link: string = "https://www.linkedin.com/in/felipe-saadi/"
-
-  function sum(n1: number, n2: number): number {
-    return n1 + n2;
-  }
-
-  function sub(n1: number, n2: number): number {
-    return n1 - n2;
-  }
-  
-  function mult(n1: number, n2: number): number {
-    return n1 * n2;
-  }
-
-  function div(n1: number, n2: number): number {
-    return n1 / n2; 
-  }
-  
   return (
     <div>
-      <div>{`Nome: ${name}`}</div>
-      <div>{`Sobrenome: ${lastName}`}</div>
-      <div>Sum: {sum(n1,n2)}</div>
-      <div>Sub: {sub(n1,n2)}</div>
-      <div>Mult: {mult(n1,n2)}</div>
-      <div>Div: {div(n1,n2).toFixed(1)}</div>
-
-      <div>
-        Link para o meu Linkedin:
-        <a href={link} target={"blank"}>Felipe Saadi | LinkedIn</a>
-      </div>
+      <Header/>
+      <Calc/>
     </div>
   )
 }
---
+++ React/Modulo 1/react-vite-first/src/components/Linkedin.tsx
@@ -0,0 +1,12 @@
+const LinkedIn = () => {
+    let link: string = "https://www.linkedin.com/in/felipe-saadi/"
+
+    return (
+        <div>
+            Link para o meu Linkedin:
+            <a href={link} target={"blank"}>Felipe Saadi | LinkedIn</a>
+        </div>
+    )
+}
+
+export default LinkedIn
