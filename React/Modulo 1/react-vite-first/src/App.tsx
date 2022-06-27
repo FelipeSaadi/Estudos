d87a5740f1c46efcafd2c9412c074c38885cdd9f
Felipe Saadi <54749257+FelipeSaadi@users.noreply.github.com>
2022-06-27 20:52:51 -0300

    Estilizando com stylesheet no React

--- React/Modulo 1/react-vite-first/src/App.tsx
+++ React/Modulo 1/react-vite-first/src/App.tsx
@@ -1,41 +1,10 @@
 import React, { useState } from 'react';
+import Button from './components/Button';
 
 const App = () => {
-  let [show, setShow] = useState(false);
-  let [clicked, setClicked] = useState(false);
-  let [padding, setPadding] = useState(0);
-
-  const handleClick = () => {
-    setShow(!show);
-    setClicked(!clicked);
-    setPadding(padding + 1)
-  }
-
-  let buttonStyle;
-
-  clicked ? buttonStyle = {
-    backgroundColor: "grey",
-    color: "white",
-    padding
-  } : buttonStyle = {
-    backgroundColor: "red",
-    color: "white",
-    padding
-  }
-
   return (
     <div>
-      {show ? (
-        <>
-          <p>Meu nome é Felipe Saadi.</p>
-          <button style={buttonStyle} onClick={handleClick}>Não mostrar</button>
-        </>
-      ) : (
-        <>
-          <p>Nada a mostrar.</p>
-          <button style={buttonStyle} onClick={handleClick}>Mostrar</button>
-        </>
-      )}
+      <Button />
     </div>
   )
 }
---
+++ React/Modulo 1/react-vite-first/src/components/Button/index.tsx
@@ -0,0 +1,12 @@
+import './styles.css'
+
+const Button = () => {
+    return (
+        <div className='square'>
+            <button className='button'>Botão</button>
+            <p className='legend'>Legenda</p>
+        </div>
+    )
+}
+
+export default Button
