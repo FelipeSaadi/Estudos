087a78f709f9f041305945182db45e6d6683d26e
Felipe Saadi <54749257+FelipeSaadi@users.noreply.github.com>
2022-06-26 23:30:25 -0300

    Usando State React

--- React/Modulo 1/react-vite-first/src/App.tsx
+++ React/Modulo 1/react-vite-first/src/App.tsx
@@ -1,22 +1,25 @@
 import Header from './components/Header'
 import Calc from './components/Calc'
 import Photo from './components/Photo'
+import { useState } from 'react';
 
 const App = () => {
+  // const [number, setNumber] = useState(0);
+  const [name, setName] = useState('Felipe');
 
   const handleButtonClick = () => {
-    alert("O botão foi clicado!");
+    setName('Saadi');
   }
 
   return (
     <div>
-      <Header title = "Título da Página"/>
-      <Calc/>
+      <Header title="Título da Página" />
+      <Calc />
 
       <Photo legend="Google">
         <img src="https://www.google.com.br/google.jpg" alt="" />
       </Photo>
-
+      Me chamo {name}.
       <button onClick={handleButtonClick}>Clique aqui</button>
     </div>
   )
