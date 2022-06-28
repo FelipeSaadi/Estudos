286f15e7b3764e56fe493d8c6bf7c86b5a8bea51
Felipe Saadi <54749257+FelipeSaadi@users.noreply.github.com>
2022-06-28 15:58:38 -0300

    React - Instalando e Configurando o Tailwind CSS

--- React/Modulo 1/react-vite-first/src/App.tsx
+++ React/Modulo 1/react-vite-first/src/App.tsx
@@ -4,6 +4,7 @@ import * as C from './AppStyles'
 
 const App = () => {
   return (
+    <div className='bg-black'>
       <C.Container bg="grey">
         <span>Texto do componente</span>
 
@@ -11,6 +12,7 @@ const App = () => {
         <C.Button bg="#ff0000" small>Clique aqui</C.Button>
         <C.Button bg="#00ff00" small>Clique aqui</C.Button>
       </C.Container>
+    </div>
   )
 }
 
