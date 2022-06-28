55116a9886de375ebbcd6bed7a96a5145b7029c4
Felipe Saadi <54749257+FelipeSaadi@users.noreply.github.com>
2022-06-28 19:15:10 -0300

    React - Aplicando TailwindCSS em estados
    Estados como hover, active, focus e etc...

--- React/Modulo 1/react-vite-first/src/App.tsx
+++ React/Modulo 1/react-vite-first/src/App.tsx
@@ -4,9 +4,13 @@ import * as C from './AppStyles'
 
 const App = () => {
   return (
-    <div className='bg-blue-900 text-white px-10 py-5 text-2xl font-bold flex flex-col text-center md:flex-row md:items-center md:text-green-400 lg:text-red-400 md:justify-between'>
-      Texto da DIV
-      <button className='bg-green-500 rounded-xl p-2 mt-3 md:ml-5 md:mt-0 lg:text-white'>Clique aqui</button>
+    <div className='bg-blue-300 p-5'>
+      <input
+        className='outline-none border border-transparent bg-white p-3 rounded focus:ring-1 focus:ring-blue-700'
+        type="text"
+        placeholder='Digite alguma coisa'
+      />
+      <button className='mx-3 bg-blue-500 p-3 rounded text-white font-bold hover:bg-blue-700'>Clique aqui</button>
     </div>
 
     // <C.Container bg="grey">
