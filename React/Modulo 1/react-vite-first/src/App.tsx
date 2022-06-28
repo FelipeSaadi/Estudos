22bfdd7159d0bc10da2104847e132d906e720d66
Felipe Saadi <54749257+FelipeSaadi@users.noreply.github.com>
2022-06-28 18:55:54 -0300

    React - Responsividade com TailwindCSS

--- React/Modulo 1/react-vite-first/src/App.tsx
+++ React/Modulo 1/react-vite-first/src/App.tsx
@@ -4,9 +4,9 @@ import * as C from './AppStyles'
 
 const App = () => {
   return (
-    <div className='bg-blue-900 text-white pt-7 pb-5 text-2xl font-bold'>
+    <div className='bg-blue-900 text-white px-10 py-5 text-2xl font-bold flex flex-col text-center md:flex-row md:items-center md:text-green-400 lg:text-red-400 md:justify-between'>
       Texto da DIV
-      <button className='bg-green-500 ml-5 rounded-xl p-2'>Clique aqui</button>
+      <button className='bg-green-500 rounded-xl p-2 mt-3 md:ml-5 md:mt-0 lg:text-white'>Clique aqui</button>
     </div>
 
     // <C.Container bg="grey">
