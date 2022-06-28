6a9f23fb163c3ed3ec406e38b7d953e4db39b172
Felipe Saadi <54749257+FelipeSaadi@users.noreply.github.com>
2022-06-28 14:57:56 -0300

    Estilização dinâmica usando Styled Components

--- React/Modulo 1/react-vite-first/src/App.tsx
+++ React/Modulo 1/react-vite-first/src/App.tsx
@@ -4,9 +4,10 @@ import * as C from './AppStyles'
 
 const App = () => {
   return (
-      <C.Container>
+      <C.Container bg="grey">
         Texto do componente
-        <C.Button>Clique aqui</C.Button>
+        <C.Button bg="#ff0000" small>Clique aqui</C.Button>
+        <C.Button bg="#00ff00" small>Clique aqui</C.Button>
       </C.Container>
   )
 }
--- React/Modulo 1/react-vite-first/src/AppStyles.ts
+++ React/Modulo 1/react-vite-first/src/AppStyles.ts
@@ -1,14 +1,24 @@
 import styled from 'styled-components'
 
-export const Container = styled.div`
-    background-color: blue;
+type ContainerProps = {
+    bg: string;
+}
+
+export const Container = styled.div<ContainerProps>`
+    background-color: ${props => props.bg};
     color: white;
     font-weight: 600;
     padding: 20px;
 `;
 
-export const Button = styled.button`
-font-size: 40;
-border-radius: 10px;
-margin-left: 10px;
+type ButtonProps = {
+    bg: string;
+    small?: boolean;
+}
+
+export const Button = styled.button<ButtonProps>`
+    font-size: ${ props => props.small ? '15px' : '30px'};
+    border-radius: 10px;
+    margin-left: 10px;
+    background-color: ${ props => props.bg}
 `;
