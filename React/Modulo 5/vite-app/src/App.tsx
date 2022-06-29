59e8a0bcad8caa955e639cd67c56096be8362ffc
Felipe Saadi <54749257+FelipeSaadi@users.noreply.github.com>
2022-06-29 19:43:20 -0300

    React - Adicionado o try catch na aplicação

--- React/Modulo 5/vite-app/src/App.tsx
+++ React/Modulo 5/vite-app/src/App.tsx
@@ -10,34 +10,52 @@ const App = () => {
     loadMovies();
   }, [])
 
+  // const loadMovies = () => {
+  //   fetch("https://api.b7web.com.br/cinema/").then((response) => {
+  //     return response.json();
+  //   }).then((json) => {
+  //     setMovies(json);
+  //     setLoading(false)
+  //   }).catch(err => {
+  //     setLoading(false);
+  //   })
+  // }
+
   const loadMovies = async () => {
-    setLoading(true);
+    try {
+      setLoading(true);
     let response = await fetch("https://api.b7web.com.br/cinema/");
     let json = await response.json();
     setLoading(false);
     setMovies(json);
+    } catch(err) {
+      setLoading(false);
+    }
   }
 
-  return (
-    <div>
-      {loading &&
-        <div>Carregando...</div>
-      }
-      {!loading &&
-        <>
-          Total de Filmes: {movies.length}
-          <div className="grid grid-cols-6 gap-3">
-            {movies.map((movie, index) => (
-              <div key={index}>
-                <img src={movie.avatar} alt="" width={150} />
-                {movie.titulo}
-              </div>
-            ))}
-          </div>
-        </>
-      }
-    </div>
-  )
+return (
+  <div>
+    {loading &&
+      <div>Carregando...</div>
+    }
+    {!loading && movies.length > 0 &&
+      <>
+        Total de Filmes: {movies.length}
+        <div className="grid grid-cols-6 gap-3">
+          {movies.map((movie, index) => (
+            <div key={index}>
+              <img src={movie.avatar} alt="" width={150} />
+              {movie.titulo}
+            </div>
+          ))}
+        </div>
+      </>
+    }
+    {!loading && movies.length === 0 &&
+      <div>Tente novamente mais tarde.</div>
+    }
+  </div>
+)
 }
 
 export default App
