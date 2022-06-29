a9d9ef9e22f2967595d8477168a0aea9db7a9a4b
Felipe Saadi <54749257+FelipeSaadi@users.noreply.github.com>
2022-06-29 19:30:00 -0300

    React - Criado um loading para a aplicação

--- React/Modulo 5/vite-app/src/App.tsx
+++ React/Modulo 5/vite-app/src/App.tsx
@@ -4,36 +4,38 @@ import { Movie } from './types/Movies';
 
 const App = () => {
   const [movies, setMovies] = useState<Movie[]>([]);
+  const [loading, setLoading] = useState(true);
 
-  /*
-  const loadMovies = () => {
-    fetch("https://api.b7web.com.br/cinema/").then((response) => {
-      return response.json();
-    }).then((json) => {
-      setMovies(json);
-    })
-  }
-  */
+  useEffect(() => {
+    loadMovies();
+  }, [])
 
   const loadMovies = async () => {
+    setLoading(true);
     let response = await fetch("https://api.b7web.com.br/cinema/");
     let json = await response.json();
+    setLoading(false);
     setMovies(json);
   }
 
   return (
     <div>
-      <button className="block bg-blue-400 p-2 rounded" onClick={loadMovies}>Carregar Filmes</button>
-      Total de Filmes: {movies.length}
-
-      <div className="grid grid-cols-6 gap-3">
-        {movies.map((movie, index) => (
-          <div key={index}>
-            <img src={movie.avatar} alt="" width={150} />
-            {movie.titulo}
+      {loading &&
+        <div>Carregando...</div>
+      }
+      {!loading &&
+        <>
+          Total de Filmes: {movies.length}
+          <div className="grid grid-cols-6 gap-3">
+            {movies.map((movie, index) => (
+              <div key={index}>
+                <img src={movie.avatar} alt="" width={150} />
+                {movie.titulo}
+              </div>
+            ))}
           </div>
-        ))}
-      </div>
+        </>
+      }
     </div>
   )
 }
