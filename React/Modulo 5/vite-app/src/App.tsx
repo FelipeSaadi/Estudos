9b52974e9a430e2b774297fa5d637bb4e8971767
Felipe Saadi <54749257+FelipeSaadi@users.noreply.github.com>
2022-06-29 18:54:17 -0300

    React - Renderizando a resposta do get (JSON)

--- React/Modulo 5/vite-app/src/App.tsx
+++ React/Modulo 5/vite-app/src/App.tsx
@@ -1,21 +1,31 @@
 import { ChangeEvent, useEffect, useState } from 'react'
 import './App.css'
+import { Movie } from './types/Movies';
 
 const App = () => {
-  const [movies, setMovies] = useState([]);
+  const [movies, setMovies] = useState<Movie[]>([]);
 
   const loadMovies = () => {
     fetch("https://api.b7web.com.br/cinema/").then((response) => {
-        return response.json();
-      }).then((json) => {
-        setMovies(json);
-      })
+      return response.json();
+    }).then((json) => {
+      setMovies(json);
+    })
   }
 
   return (
     <div>
-      <button onClick={loadMovies}>Carregar Filmes</button>
+      <button className="block bg-blue-400 p-2 rounded" onClick={loadMovies}>Carregar Filmes</button>
       Total de Filmes: {movies.length}
+
+      <div className="grid grid-cols-6 gap-3">
+        {movies.map((movie, index) => (
+          <div key={index}>
+            <img src={movie.avatar} alt="" width={150} />
+            {movie.titulo}
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
