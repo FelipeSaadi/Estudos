a533b3bac7f4503b7bddaebdc6c7585e879cf3db
Felipe Saadi <54749257+FelipeSaadi@users.noreply.github.com>
2022-06-29 00:17:15 -0300

    React - Criada a lógica inicial do resultado

--- React/Modulo 4/react_calc_imc/src/App.tsx
+++ React/Modulo 4/react_calc_imc/src/App.tsx
@@ -1,17 +1,19 @@
 import { useState } from 'react';
 import styles from './App.module.css'
 import poweredImage from './assets/powered.png';
-import { levels, calculateImc } from './helpers/imc';
-import {  GridItem  } from './components/GridItem'
+import { levels, calculateImc, Level } from './helpers/imc';
+import { GridItem } from './components/GridItem'
 
 function App() {
   const [heightField, setHeightField] = useState<number>(0);
   const [weightField, setWeightFeild] = useState<number>(0);
+  const [toShow, setToShow] = useState<Level | null>(null);
 
   const handleCalculateButton = () => {
-    if(heightField && weightField) {
-      console.log(calculateImc(heightField, weightField))
+    if (heightField && weightField) {
+      setToShow(calculateImc(heightField, weightField));
     } else {
+      setToShow(null)
       alert("Digite todos os campos.")
     }
   }
@@ -28,28 +30,36 @@ function App() {
           <h1>Calcule o seu IMC.</h1>
           <p>IMC é a sigla para Índice de Massa Corpórea, parâmetro adotado pela Organização Mundial de Saúde para calcular o peso ideal de cada pessoa.</p>
 
-          <input 
+          <input
             type="number"
             placeholder="Digite a sua altura. Ex: 1.5 (em metros)"
-            value={ heightField > 0 ? heightField : ''}
+            value={heightField > 0 ? heightField : ''}
             onChange={e => setHeightField(parseFloat(e.target.value))}
           />
 
-          <input 
+          <input
             type="number"
             placeholder="Digite o seu peso. Ex: 75.2 (em kg)"
-            value={ weightField > 0 ? weightField : ''}
+            value={weightField > 0 ? weightField : ''}
             onChange={e => setWeightFeild(parseFloat(e.target.value))}
           />
 
           <button onClick={handleCalculateButton}>Calcular</button>
         </div>
         <div className={styles.rightSide}>
-          <div className={styles.grid}>
-            { levels.map((item, key) => (
-              <GridItem key={key} data={item}></GridItem>
-            )) }
-          </div>
+          {!toShow &&
+            <div className={styles.grid}>
+              {levels.map((item, key) => (
+                <GridItem key={key} data={item}></GridItem>
+              ))}
+            </div>
+          }
+          {toShow &&
+            <div className={styles.rightResult}>
+              <div className={styles.rightArrow}></div>
+              <GridItem data={toShow} />
+            </div>
+          }
         </div>
       </div>
     </div>
